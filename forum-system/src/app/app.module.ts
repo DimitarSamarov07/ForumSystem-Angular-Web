0af12d57bff93bc5de38f9d907391d874ef6ac76
DimitarSamarov07 <samarovdimitar@gmail.com>
2020-12-04 23:42:06 +0200

    Project generated + some work done to the home page's HTML.

---
+++ forum-system/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './core/header/header.component';
+import {FooterComponent} from './core/footer/footer.component';
+import {CoreModule} from "./core/core.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, HeaderComponent, FooterComponent],
+})
+export class AppModule {
+}
---
+++ forum-system/src/assets/site.js
@@ -0,0 +1,25 @@
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// HOme page forum view button
+function change() {
+  var elem = document.getElementById("myButton1");
+  if (elem.value === "Hide list of categories") elem.value = "Show list of categories";
+  else elem.value = "Hide list of categories";
+}
