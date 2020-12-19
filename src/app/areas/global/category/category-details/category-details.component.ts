c2e3f4f1895093391446457336a0f6bea6af164b
Dimitar Samarov <samarovdimitar@gmail.com>
2020-12-19 09:37:11 +0200

    Visual fixes. Voting fixed.

--- src/app/areas/global/category/category-details/category-details.component.ts
+++ src/app/areas/global/category/category-details/category-details.component.ts
@@ -6,6 +6,8 @@ import ICategoryDetailsList from "../../../../shared/interfaces/ICategoryDetails
 import {ActivatedRoute} from "@angular/router";
 import * as moment from "moment";
 import {PageEvent} from "@angular/material/paginator";
+import IUser from "../../../../shared/interfaces/IUser";
+import {UserService} from "../../../../user/user.service";
 
 @Component({
   selector: 'app-category-details',
@@ -18,11 +20,14 @@ export class CategoryDetailsComponent implements OnInit {
   categoryId: string;
   totalDataLength: number;
   category: ICategoryDetailsList;
+  currUser: IUser
+  paginating = true;
 
   constructor(
     private fb: FormBuilder,
     private postService: PostService,
-    private activatedRoute: ActivatedRoute) {
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService) {
     this.categoryId = activatedRoute.snapshot.params.categoryId;
     this.searchForm = fb.group({
       query: ["", [Validators.required, Validators.minLength(2)]]
@@ -30,8 +35,11 @@ export class CategoryDetailsComponent implements OnInit {
   }
 
   async ngOnInit() {
+    this.currUser = await this.userService.getCurrUser();
     this.category = await this.postService.paginatePostsFromCategory(this.categoryId, 0, 10)
     this.totalDataLength = await this.postService.getCountOfCategoryPosts(this.categoryId);
+
+    this.paginating = false;
   }
 
   public trackItem(index: number, item: IPost) {
@@ -42,17 +50,15 @@ export class CategoryDetailsComponent implements OnInit {
     target.value = "";
   }
 
-  onSearchFormSubmit(form) {
-
-  }
-
   getConvertedDate(created: string) {
     return moment(created).format("DD/MM/YYYY hh:mm:ss");
   }
 
   async onPaginatorEvent($event: PageEvent) {
+    this.paginating = true;
     const page = $event.pageIndex;
     const size = $event.pageSize;
     this.category = await this.postService.paginatePostsFromCategory(this.categoryId, page, size);
+    this.paginating = false;
   }
 }
--- src/app/services/vote/vote.service.ts
+++ src/app/services/vote/vote.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import IVote from "../../shared/interfaces/IVote";
 import IUser from "../../shared/interfaces/IUser";
+import {PostService} from "../post/post.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,32 +11,34 @@ export class VoteService {
   private voteStore = Backendless.Data.of("Votes");
   private userStore = Backendless.Data.of("Users")
 
-  constructor() {
+  constructor(private postService: PostService) {
   }
 
   async registerVote(postId, userId, polarity: boolean) {
     const voteObjInDB = await this.checkUserVote(userId, postId);
+    const post = await this.postService.retrievePost(postId)
+    const authorId = post.author.objectId;
 
     if (voteObjInDB.length > 0) {
-      return await this.changeVote(voteObjInDB[0], polarity)
+      return await this.changeVote(voteObjInDB[0], polarity, authorId)
     }
 
     const vote = await this.voteStore.save({polarity: polarity});
     await this.voteStore.setRelation(vote, "user", [{objectId: userId}]);
     await this.voteStore.setRelation(vote, "post", [{objectId: postId}])
 
-    const user = await this.userStore.findById<IUser>(userId);
+    const user = await this.userStore.findById<IUser>(authorId);
     const interpret = polarity ? 1 : -1;
     user.karmaPoints += interpret;
 
     await this.userStore.save(user);
   }
 
-  async changeVote(voteObj, newPolarity) {
+  async changeVote(voteObj, newPolarity, authorId) {
     voteObj.polarity = newPolarity;
     await this.voteStore.save(voteObj);
 
-    const user = await this.userStore.findById<IUser>(voteObj.user.objectId);
+    const user = await this.userStore.findById<IUser>(authorId);
     const interpret = newPolarity ? 2 : -2;
     user.karmaPoints += interpret;
 
